87b9e0e5878e7faf3127bf2909a7453c01a31b51
Mikael Larsson <mikael.larsson@epiroc.com>
2020-10-27 17:42:00 +0100

    Initial commit

---
+++ app/src/main.cpp
@@ -0,0 +1,86 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include "glad/glad.h"
+
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+
+static void glfw_error_callback(int error, const char* description)
+{
+   std::cerr << "GLFW error [" << error << "]: " << description << "\n";
+}
+
+int main (int argc, char** argv)
+{
+   glfwSetErrorCallback(glfw_error_callback);
+
+   if (!glfwInit()) {
+      std::exit(EXIT_FAILURE);
+   }
+
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+   GLFWwindow* window = glfwCreateWindow(1280, 720, "Meshup", NULL, NULL);
+   if (!window) {
+      std::exit(EXIT_FAILURE);
+   }
+
+   glfwMakeContextCurrent(window);
+   glfwSwapInterval(1);
+
+   if (gladLoadGL() == 0) {
+      std::cerr << "Failed to initialize OpenGL loader\n";
+      std::exit(EXIT_FAILURE);
+   }
+
+   ImGui::CreateContext();
+   ImGui::StyleColorsDark();
+   ImGui_ImplGlfw_InitForOpenGL(window, true);
+   ImGui_ImplOpenGL3_Init("#version 330" /* glsl version string*/);
+
+   ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
+
+   while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+
+      static float f = 0.0f;
+      static int counter = 0;
+
+      ImGui::Begin("Hello");
+
+      ImGui::Text("World!");
+
+      ImGui::End();
+
+      // Rendering
+      ImGui::Render();
+
+      int display_w, display_h;
+      glfwGetFramebufferSize(window, &display_w, &display_h);
+
+      glViewport(0, 0, display_w, display_h);
+      glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+      glfwSwapBuffers(window);
+   }
+
+   ImGui_ImplOpenGL3_Shutdown();
+   ImGui_ImplGlfw_Shutdown();
+   ImGui::DestroyContext();
+
+   glfwDestroyWindow(window);
+   glfwTerminate();
+
+   return 0;
+}
---
+++ external/external.cmake
@@ -0,0 +1,75 @@
+include(FetchContent)
+set(FETCHCONENT_QUITE FALSE)
+
+#
+# OpenGL
+#
+
+find_package(OpenGL REQUIRED COMPONENTS OpenGL)
+
+#
+# GLAD
+#
+
+FetchContent_Declare(glad
+   GIT_REPOSITORY https://github.com/Dav1dde/glad.git
+   GIT_TAG a5ca31c88a4cc5847ea012629aff3690f261c7c4 # 0.1.34
+)
+
+set(GLAD_PROFILE "core" CACHE STRING "OpenGL profile")
+set(GLAD_API "gl=3.3" CACHE STRING "API type/version pairs, like \"gl=3.2,gles=\", no version means latest")
+set(GLAD_GENERATOR "c" CACHE STRING "Language to generate the binding for")
+
+FetchContent_MakeAvailable(glad)
+
+#
+# GLFW
+#
+
+FetchContent_Declare(glfw
+   GIT_REPOSITORY https://github.com/glfw/glfw
+   GIT_TAG 0a49ef0a00baa3ab520ddc452f0e3b1e099c5589 # 3.3.2
+)
+
+set(GLFW_BUILD_EXAMPLES OFF CACHE INTERNAL "Build the GLFW example programs")
+set(GLFW_BUILD_TESTS OFF CACHE INTERNAL "Build the GLFW test programs")
+set(GLFW_BUILD_DOCS OFF CACHE INTERNAL "Build the GLFW documentation")
+set(GLFW_INSTALL OFF CACHE INTERNAL "Generate installation target")
+
+FetchContent_MakeAvailable(glfw)
+
+
+#
+# IMGUI
+#
+
+FetchContent_Declare(imgui
+   GIT_REPOSITORY https://github.com/ocornut/imgui.git
+   GIT_TAG e5cb04b132cba94f902beb6186cb58b864777012 # v1.79
+)
+
+FetchContent_GetProperties(imgui)
+if(NOT imgui_POPULATED)
+   FetchContent_Populate(imgui)
+
+  set(IMGUI_SOURCES
+     "${imgui_SOURCE_DIR}/imgui.cpp"
+     "${imgui_SOURCE_DIR}/imgui_draw.cpp"
+     "${imgui_SOURCE_DIR}/imgui_widgets.cpp"
+     "${imgui_SOURCE_DIR}/examples/imgui_impl_glfw.cpp"
+     "${imgui_SOURCE_DIR}/examples/imgui_impl_opengl3.cpp")
+
+  set(IMGUI_LIB_NAME "imgui-glfw-lib")
+
+  add_library(${IMGUI_LIB_NAME} ${IMGUI_SOURCES})
+
+  add_definitions(-DIMGUI_IMPL_OPENGL_LOADER_GLAD)
+
+  target_include_directories(${IMGUI_LIB_NAME}
+    PUBLIC
+      ${imgui_SOURCE_DIR}
+      ${imgui_SOURCE_DIR}/examples
+  )
+
+  target_link_libraries(${IMGUI_LIB_NAME} OpenGL::OpenGL glad glfw)
+endif()
