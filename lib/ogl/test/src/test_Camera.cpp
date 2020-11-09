7c5aa9b4d41a0143c9d6b0481817dabafa702bff
Mikael Larsson <mikael.larsson@epiroc.com>
2020-11-09 21:32:32 +0100

    Add stub for a Camera class

---
+++ lib/ogl/test/src/test_Camera.cpp
@@ -0,0 +1,19 @@
+#include "meshup/ogl/Camera.h"
+
+#include "catch2/catch.hpp"
+
+#include "glm/gtc/matrix_transform.hpp"
+
+using namespace meshup::ogl;
+
+TEST_CASE("InitiateCamera") {
+   glm::vec3 position(0.f, 0.f, 5.f);
+   glm::vec3 target(0.f, 0.f, 0.f);
+   glm::vec3 up(0.f, 1.f, 0.f);
+
+   glm::mat4 expectedViewMatrix = glm::lookAt(position, target, up);
+
+   Camera camera(position, target, up);
+
+   REQUIRE(expectedViewMatrix == camera.getViewMatrix());
+}
